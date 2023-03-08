39db7d1fc0cdae0063d98eb19b89b5bd878e23f0
HatsuneMona <songmiao39@foxmail.com>
2023-03-08 10:11:32 +0800

    feat: 优化 usercenter 的路由逻辑

--- backend/routers/userCenter.go
+++ backend/routers/userCenter.go
@@ -3,25 +3,17 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"monaToolBox/app/userCenter/controllers"
-	"monaToolBox/middleware"
 )
 
 func userCenterRouter(root *gin.RouterGroup) {
 	// userCenter
 
-	//root.GET("/ping", func(c *gin.Context) {
-	//	c.String(http.StatusOK, "pong!")
-	//})
-
 	root.POST("/register", controllers.Register)
 	root.POST("/login", controllers.Login)
 
-	userLoggedGroup := root.Group("").Use(middleware.JwtAuth)
-	userLoggedGroup.POST("/logout", controllers.Logout)
-	userLoggedGroup.GET("/userInfo", controllers.GetUserInfo)
+}
 
-	//userLoggedGroup := root.Group(":userId").Use(middleware.JwtAuth)
-	//userLoggedGroup.GET("/userInfo", controllers.GetUserInfo)
-	//userLoggedGroup.POST("/logout", controllers.Logout)
-
+func userCenterAdminRouter(root *gin.RouterGroup) {
+	root.POST("/logout", controllers.Logout)
+	root.GET("/userInfo", controllers.GetUserInfo)
 }
